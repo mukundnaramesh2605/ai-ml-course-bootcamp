4846d8317938a729b7b97e52dc4cbbd152217f61
mukundan.ramesh <mukundanramesh26@gmail.com>
2026-06-15 08:56:31 +0530

    Numpy Hands and Tutorial

---
+++ numpy/javascript/increment_operator.js
@@ -0,0 +1,19 @@
+var first = "Hello"
+var second = "World"
+console.log(first + " " + second) // Hello World
+
+var a = 5
+console.log(a++) // 5 (returns a, then increments)
+console.log(a) // 6 (a is now incremented)
+
+var b = 10
+console.log(++b) // 11 (increments b, then returns it)
+console.log(b) // 11 (b is now incremented)
+
+var c = 15
+console.log(c--) // 15 (returns c, then decrements)
+console.log(c) // 14 (c is now decremented)
+
+var d = 20
+console.log(--d) // 19 (decrements d, then returns it)
+console.log(d) // 19 (d is now decremented)
