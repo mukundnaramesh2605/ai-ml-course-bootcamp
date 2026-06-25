bb809a5f18aefc02ed7e7a38b8649e9507e395f7
mukundan.ramesh <mukundanramesh26@gmail.com>
2026-06-25 10:41:14 +0530

    -- ===================================================== -- QUESTION 1 -- Difficulty: ⭐ -- -- Display the employee_name and salary
    -- =====================================================
    -- QUESTION 1
    -- Difficulty: ⭐
    --
    -- Display the employee_name and salary

---
+++ SQL/3.SQL_Practice.sql
@@ -0,0 +1,12 @@
+-- =====================================================
+-- QUESTION 1
+-- Difficulty: ⭐
+--
+-- Display the employee_name and salary
+-- of all employees.
+--
+-- Expected Columns:
+-- employee_name | salary
+-- =====================================================
+
+SELECT employee_name,SALARY from  employees;
