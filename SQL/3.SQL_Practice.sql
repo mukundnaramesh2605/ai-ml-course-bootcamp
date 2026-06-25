920683f9cc4cd4affa3c13c4408b3bcda1d7a224
mukundan.ramesh <mukundanramesh26@gmail.com>
2026-06-25 10:45:27 +0530

    -- ===================================================== -- QUESTION 5 -- Difficulty: ⭐⭐ -- -- Display the employee_name, salary, -- and bonus of employees whose salary -- is between 60000 and 90000. -- -- Expected Columns: -- employee_name | salary | bonus -- =====================================================
    -- =====================================================
    -- QUESTION 5
    -- Difficulty: ⭐⭐
    --
    -- Display the employee_name, salary,
    -- and bonus of employees whose salary
    -- is between 60000 and 90000.
    --
    -- Expected Columns:
    -- employee_name | salary | bonus
    -- =====================================================

--- SQL/3.SQL_Practice.sql
+++ SQL/3.SQL_Practice.sql
@@ -50,4 +50,18 @@ select employee_name,department,salary from employees where salary> 75000;
 -- =====================================================
 SELECT employee_name,city from employees where city='Chennai';
 
+-- =====================================================
+-- QUESTION 5
+-- Difficulty: ⭐⭐
+--
+-- Display the employee_name, salary,
+-- and bonus of employees whose salary
+-- is between 60000 and 90000.
+--
+-- Expected Columns:
+-- employee_name | salary | bonus
+-- =====================================================
+
+
+select employee_name,salary,bonus from employees where salary between 60000 and 90000;
 
